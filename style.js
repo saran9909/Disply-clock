137e79dc50b9b0070c479bd8145ac6f784f30c55
saran9909 <sarancharan411@gmail.com>
2024-09-24 20:58:31 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,38 +1,43 @@
-function updateTime() {
-    
+function newYearCountdown() {
+    const newYearDay = new Date('January 1, 2022 00:00');
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const diff = newYearDay - now;
+    console.log(diff);
 
-    
-    let formattedHours = hours % 12 || 12; 
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+    const msSeconds = 1000;
+    const msMinutes = 60 * 1000;
+    const msHours = 60 * 60 * 1000;
+    const msDays = 24 * 60 * 60 * 1000;
 
-    
-    document.getElementById('currentTime').textContent =`${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm};` 
-        
 
-    
-    const nextYear = new Date(now.getFullYear() + 1, 0, 1);
-    const timeDiff = nextYear - now;
+    const displayDay = Math.floor(diff / msDays);
+    document.querySelector('.days').textContent = displayDay;
+    const displayHours = Math.floor((diff % msDays) / msHours);
+    document.querySelector('.hours').textContent = displayHours;
+    const displayMinutes = Math.floor((diff % msHours) / msMinutes);
+    document.querySelector('.minutes').textContent = displayMinutes;
+    const displaySeconds = Math.floor((diff % msMinutes) / msSeconds);
+    document.querySelector('.seconds').textContent = displaySeconds;
 
-    
-    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    const hoursLeft = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutesLeft = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const secondsLeft = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-    
-    document.getElementById('countdown').textContent = 
-        `${days}days   ${hoursLeft}hrs    ${minutesLeft}min ${secondsLeft}sec
-        `;
-
-    
-    setTimeout(updateTime, 1000);
+    if (diff <= 0) {
+        document.querySelector('.days').textContent = 0;
+        document.querySelector('.hours').textContent = 0;
+        document.querySelector('.minutes').textContent = 0;
+        document.querySelector('.seconds').textContent = 0;
+        clearInterval(timerID);
+    }
 }
 
+let timerId = setInterval(newYearCountdown, 1000);
+newYearCountdown()
 
-updateTime();
+
+const btn = document.querySelector('#play');
+btn.addEventListener('click', () => {
+    document.querySelector('#myAudio').play();
+})
+
+const btnPause = document.querySelector('#pause');
+btnPause.addEventListener('click', () => {
+    document.querySelector('#myAudio').pause();
+})
